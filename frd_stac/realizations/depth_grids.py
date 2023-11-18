61f35639effe4eca2308e7b68e44db0ba99c4ff8
Seth Lawler <slawler@dewberry.com>
2023-11-18 08:32:43 -0500

    clear dev

--- frd_stac/realizations/depth_grids.py
+++
@@ -1,9 +0,0 @@
-col_map = {
-    "id": "depth-grids",
-    "description": "(Tiled) depth grids from the HEC-RAS model output for this realization - simulation",
-    "extensions": [
-        "https://stac-extensions.github.io/projection/v1.0.0/schema.json",
-        "https://stac-extensions.github.io/osc/v1.0.0-rc.3/schema.json",
-        "https://stac-extensions.github.io/processing/v1.1.0/schema.json",
-    ],
-}
--- frd_stac/realizations/ressim.py
+++
@@ -1,9 +0,0 @@
-col_map = {
-    "id": "reservoirs",
-    "description": "HEC-RESSIM  model inputs and outputs specific to this realization - simulation",
-    "extensions": [
-        "https://stac-extensions.github.io/projection/v1.0.0/schema.json",
-        "https://stac-extensions.github.io/osc/v1.0.0-rc.3/schema.json",
-        "https://stac-extensions.github.io/processing/v1.1.0/schema.json",
-    ],
-}
