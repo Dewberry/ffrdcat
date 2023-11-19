1c084b46e8045ecdc81dd9abd2a262a281959151
Seth Lawler <slawler@dewberry.com>
2023-11-19 17:55:05 -0500

    add plugin scripts

---
+++ ffrdcat/plugins/zip_reader/main.py
@@ -0,0 +1,17 @@
+import sys
+from process_zip import main, plugin_params
+from papipyplug import parse_input, print_results, plugin_logger
+
+
+if __name__ == "__main__":
+    # Start plugin logger
+    plugin_logger()
+
+    # Read, parse, and verify input parameters
+    input_params = parse_input(sys.argv, plugin_params)
+
+    # Add main function here
+    results = main(input_params)
+
+    # Print Results
+    print_results(results)
